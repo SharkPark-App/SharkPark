f52ab234075edbb1da0ba5c1fd9422946136004b
Charles Milton <139507918+chuckmilton@users.noreply.github.com>
2026-03-05 10:35:17 -0800

    Revert "ML Core Modules"

--- apps/backend/prisma/migrations/20260304000000_add_semester_to_snapshots/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "occupancy_snapshots" ADD COLUMN "semester" TEXT;
-
--- DropIndex
-DROP INDEX "idx_snapshots_training";
-
--- CreateIndex
-CREATE INDEX "idx_snapshots_training" ON "occupancy_snapshots"("lot_id", "timestamp", "semester", "academic_period");
