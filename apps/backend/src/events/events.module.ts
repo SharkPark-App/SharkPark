47b2747db724f1bb8bbeefaa0a392adc1125112e
Zachary Padilla <73139549+zachfpadilla@users.noreply.github.com>
2026-05-02 05:24:10 -0700

    feat: update events module for scraper

--- apps/backend/src/events/events.module.ts
+++ apps/backend/src/events/events.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
+import { EventsScraperService } from './events-scraper.service';
 
 @Module({
   controllers: [EventsController],
-  providers: [EventsService],
-  exports: [EventsService],
+  providers: [EventsService, EventsScraperService],
+  exports: [EventsService, EventsScraperService],
 })
 export class EventsModule {}
