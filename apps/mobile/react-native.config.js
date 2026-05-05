f8ab6d4155e6cbf4c2de8b8691d45201bd64521c
Zachary Padilla <73139549+zachfpadilla@users.noreply.github.com>
2026-05-05 07:16:38 -0700

    fix: explicit android dependency (#193)
    - made `react-native-get-random-values` import explicit
    
    Co-authored-by: Charles Milton <139507918+chuckmilton@users.noreply.github.com>

--- apps/mobile/react-native.config.js
+++ apps/mobile/react-native.config.js
@@ -4,4 +4,16 @@ module.exports = {
     android: {},
   },
   assets: ['./assets/fonts'],
+  dependencies: {
+    // v2.0.0 dropped the android autolinking metadata from package.json,
+    // so the RN CLI won't detect it without this explicit config.
+    'react-native-get-random-values': {
+      platforms: {
+        android: {
+          packageImportPath: 'import org.linusu.RNGetRandomValuesPackage;',
+          packageInstance: 'new RNGetRandomValuesPackage()',
+        },
+      },
+    },
+  },
 };
